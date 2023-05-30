81596954b9072bffea5e393e8551487dc49081a3
Sam <133298123+Samuel-Kinaitis@users.noreply.github.com>
2023-05-29 21:07:08 -0400

    Add files via upload

---
+++ testRectangleSamuelKinaitis/testRectangleSamuelKinaitis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Samuel Kinaitis 10.3 
+namespace testRectangleSamuelKinaitis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Ask for date input
+            //Length
+            Console.WriteLine("Length of rectangle is");
+            float length = float.Parse(Console.ReadLine());
+            //Width
+            Console.WriteLine("Width of rectangle is");
+            float width = float.Parse(Console.ReadLine());
+
+            //Date output
+            Rectangle rect = new Rectangle(length, width);
+            Console.WriteLine(rect);
+            //Pause/exit
+            Console.WriteLine("Press enter to close the program");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ testRectangleSamuelKinaitis/testRectangleSamuelKinaitis/Rectangle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testRectangleSamuelKinaitis
+{
+    internal class Rectangle
+    {//variables and set
+        private float length = 1;
+        public float width = 1;
+
+        //constructor
+        public Rectangle(float length, float width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        //checking to make sure length is between 0 and 20
+        public float Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value <= 0.0f || value > 20.0f)
+                {
+                    this.length = 1.0f;
+                }
+                else
+                {
+                    this.length = value;
+                }
+            }
+            
+
+        }
+        //checking to make sure width is between 0 and 20
+        public float Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value <= 0.0f || value > 20.0f)
+                {
+                    this.width = 1.0f;
+                }
+                else
+                {
+                    this.width = value;
+                }
+            }
+        }
+            //Gets perimeter from length and width
+            public float Perimeter
+        {
+            get 
+            {
+                return (length + width)*2;
+            }
+        }
+        //Times length and width to get Area
+        public float Area
+        {
+            get
+            {
+                return (length * width);
+            }
+        }
+        //returns length, width, perimeter, and area.
+        public override string ToString()
+        {
+            return string.Format("Perimeter of the {0:F2} by {1:F2} rectangle is {2:F2} and the area is {3:F2}", length, width, Perimeter, Area);
+        }
+    }
+}
